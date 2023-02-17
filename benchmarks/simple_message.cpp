81c939475b7928ae2dc63b8b508c7a47c0a1a690
Javier <javi.gomez.jga@gmail.com>
2023-02-17 22:41:55 +0000

    chore: adding correct formatting settings

--- benchmarks/simple_message.cpp
+++ benchmarks/simple_message.cpp
@@ -1,82 +1,76 @@
-#include <benchmark/benchmark.h>
-#include "player_events_generated.h"
-#include "util_functions.h"
 #include "lib.h"
+#include "player_events_generated.h"
+#include <benchmark/benchmark.h>
 
 namespace benchmark {
 
-    // uint64_t get_ts() {
-    //     using namespace std::chrono;
-    //     return duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
-    // };
-
-    static void BM_GetTimestamp(benchmark::State& state) {
-        for (auto _ : state) {
-            auto ts = MyGame::get_ts();
-            benchmark::DoNotOptimize(ts);
-        }
-        state.SetItemsProcessed(state.iterations());
-    }
-
-    static void BM_PositionCreation(benchmark::State& state) {
-        for (auto _ : state) {
-            auto pos1 = MyGame::Events::Point3D(1.0f, 2.0f, 3.0f);
-            auto pos2 = MyGame::Events::Point3D(1.0f, 2.0f, 3.0f);
-            auto uuid = MyGame::Events::UUID(12L, 23L);
-            benchmark::DoNotOptimize(uuid);
-        }
-        state.SetItemsProcessed(state.iterations());
-    }
-
-    static void BM_CreatePlayerMovedEvent(benchmark::State& state) {
-        auto pos1 = MyGame::Events::Point3D(1.0f, 2.0f, 3.0f);
-        auto pos2 = MyGame::Events::Point3D(1.0f, 2.0f, 3.0f);
-        auto uuid = MyGame::Events::UUID(12L, 23L);
-        for (auto _ : state) {
-            auto event = MyGame::CreatePlayerMovedBuffer(uuid, pos1, pos2);
-            benchmark::DoNotOptimize(event);
-        }
-        state.SetItemsProcessed(state.iterations());
-    }
-
-    static void BM_SimpleCalculateDistance(benchmark::State& state) {
-        auto pos1 = MyGame::Events::Point3D(1.0f, 2.0f, 3.0f);
-        auto pos2 = MyGame::Events::Point3D(1.0f, 2.0f, 3.0f);
-        auto uuid = MyGame::Events::UUID(12L, 23L);
-
-        for (auto _ : state) {
-            auto distance = MyGame::distance(pos1, pos2);
-            benchmark::DoNotOptimize(distance);
-        }
-        state.SetItemsProcessed(state.iterations());
-    }
-
-    static void BM_CalculateDistanceBuffer(benchmark::State& state) {
-        auto pos1 = MyGame::Events::Point3D(1.0f, 2.0f, 3.0f);
-        auto pos2 = MyGame::Events::Point3D(1.0f, 2.0f, 3.0f);
-        auto uuid = MyGame::Events::UUID(12L, 23L);
-        auto eventPtr = MyGame::CreatePlayerMovedBuffer(uuid, pos1, pos2);
-
-        for (auto _ : state) {
-            auto event = MyGame::Events::GetPlayerEvent(eventPtr);
-            benchmark::DoNotOptimize(event);
-            if (event->event_type() == MyGame::Events::Event_PlayerMoved) {
-                auto playerMoved = event->event_as_PlayerMoved();
-                auto p1 = playerMoved->pos1();
-                auto p2 = playerMoved->pos2();
-                auto distance = MyGame::distance(*p1, *p2);
-                benchmark::DoNotOptimize(distance);
-            }
-        }
-        state.SetItemsProcessed(state.iterations());
-    }
-
-    BENCHMARK(BM_GetTimestamp);
-    BENCHMARK(BM_PositionCreation);
-    BENCHMARK(BM_CreatePlayerMovedEvent);
-    BENCHMARK(BM_SimpleCalculateDistance);
-    BENCHMARK(BM_CalculateDistanceBuffer);
-
+static void BM_GetTimestamp(benchmark::State &state) {
+  for (auto _ : state) {
+    auto ts = MyGame::get_ts();
+    benchmark::DoNotOptimize(ts);
+  }
+  state.SetItemsProcessed(state.iterations());
 }
+
+static void BM_PositionCreation(benchmark::State &state) {
+  for (auto _ : state) {
+    auto pos1 = MyGame::Events::Point3D(1.0f, 2.0f, 3.0f);
+    auto pos2 = MyGame::Events::Point3D(1.0f, 2.0f, 3.0f);
+    auto uuid = MyGame::Events::UUID(12L, 23L);
+    benchmark::DoNotOptimize(uuid);
+  }
+  state.SetItemsProcessed(state.iterations());
+}
+
+static void BM_CreatePlayerMovedEvent(benchmark::State &state) {
+  auto pos1 = MyGame::Events::Point3D(1.0f, 2.0f, 3.0f);
+  auto pos2 = MyGame::Events::Point3D(1.0f, 2.0f, 3.0f);
+  auto uuid = MyGame::Events::UUID(12L, 23L);
+  for (auto _ : state) {
+    auto event = MyGame::CreatePlayerMovedBuffer(uuid, pos1, pos2);
+    benchmark::DoNotOptimize(event);
+  }
+  state.SetItemsProcessed(state.iterations());
+}
+
+static void BM_SimpleCalculateDistance(benchmark::State &state) {
+  auto pos1 = MyGame::Events::Point3D(1.0f, 2.0f, 3.0f);
+  auto pos2 = MyGame::Events::Point3D(1.0f, 2.0f, 3.0f);
+  auto uuid = MyGame::Events::UUID(12L, 23L);
+
+  for (auto _ : state) {
+    auto distance = MyGame::distance(pos1, pos2);
+    benchmark::DoNotOptimize(distance);
+  }
+  state.SetItemsProcessed(state.iterations());
+}
+
+static void BM_CalculateDistanceBuffer(benchmark::State &state) {
+  auto pos1 = MyGame::Events::Point3D(1.0f, 2.0f, 3.0f);
+  auto pos2 = MyGame::Events::Point3D(1.0f, 2.0f, 3.0f);
+  auto uuid = MyGame::Events::UUID(12L, 23L);
+  auto eventPtr = MyGame::CreatePlayerMovedBuffer(uuid, pos1, pos2);
+
+  for (auto _ : state) {
+    auto event = MyGame::Events::GetPlayerEvent(eventPtr);
+    benchmark::DoNotOptimize(event);
+    if (event->event_type() == MyGame::Events::Event_PlayerMoved) {
+      auto playerMoved = event->event_as_PlayerMoved();
+      auto p1 = playerMoved->pos1();
+      auto p2 = playerMoved->pos2();
+      auto distance = MyGame::distance(*p1, *p2);
+      benchmark::DoNotOptimize(distance);
+    }
+  }
+  state.SetItemsProcessed(state.iterations());
+}
+
+BENCHMARK(BM_GetTimestamp);
+BENCHMARK(BM_PositionCreation);
+BENCHMARK(BM_CreatePlayerMovedEvent);
+BENCHMARK(BM_SimpleCalculateDistance);
+BENCHMARK(BM_CalculateDistanceBuffer);
+
+} // namespace benchmark
 
 BENCHMARK_MAIN();
--- src/lib.h
+++ src/lib.h
@@ -4,15 +4,21 @@
 #include "player_events_generated.h"
 #include <stdint.h>
 
-namespace MyGame
-{
-    uint8_t* CreatePlayerMovedBuffer(const Events::UUID& id, const Events::Point3D& ini, const Events::Point3D& end);
-    uint8_t* CreateArrayPlayerMovedBuffer(std::vector<std::tuple<Events::UUID, Events::Point3D, Events::Point3D>> players);
-    uint8_t* CreateArrayPlayerMovedBuffer(flatbuffers::FlatBufferBuilder& builder, std::vector<std::tuple<Events::UUID, Events::Point3D, Events::Point3D>> players);
-    // unit8_t* CreatePlayerMovedBuffer(const Events::UUID &id, const Events::Point3D &ini, const Events::Point3D &end, flatbuffers::FlatBufferBuilder &builder);
+namespace MyGame {
+uint8_t *CreatePlayerMovedBuffer(const Events::UUID &id,
+                                 const Events::Point3D &ini,
+                                 const Events::Point3D &end);
 
-    float distance(float x1, float y1, float z1, float x2, float y2, float z2);
-    float distance(const Events::Point3D& p1, const Events::Point3D& p2);
+using PlayerMovementTuple =
+    std::tuple<Events::UUID, Events::Point3D, Events::Point3D>;
 
-    uint64_t get_ts();
-};
+uint8_t *CreateArrayPlayerMovedBuffer(std::vector<PlayerMovementTuple> players);
+
+uint8_t *CreateArrayPlayerMovedBuffer(flatbuffers::FlatBufferBuilder &builder,
+                                      std::vector<PlayerMovementTuple> players);
+
+float distance(float x1, float y1, float z1, float x2, float y2, float z2);
+float distance(const Events::Point3D &p1, const Events::Point3D &p2);
+
+uint64_t get_ts();
+}; // namespace MyGame
